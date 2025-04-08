1a9cd85bbc4963816dcea9d063532e89540bbc76
Mateo González <minidrw@gmail.com>
2025-04-07 21:17:46 -0300

    ���

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,17 +7,9 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+output : 'server',
   adapter: vercel(),
   vite: {
     plugins: [tailwindcss()]
   },
-
-  env: {
-    schema: {
-        SHOW_BUY_BUTTON: envField.boolean({context: 'server', access: 'public'}),
-        SCORE_API_ENDPOINT: envField.string({context: 'server', access: 'public'}),
-    }
-  },
-
 });
